a4ce96883db530b2e2bb5772a3ca7e0c3ce86bd9
peela123 <peelaxv@gmail.com>
2021-02-05 18:44:57 +0700

    งานlab15

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,34 +1,85 @@
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
-#include<iomanip>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
 using namespace std;
 
-void showData(double *,int,int);
+void showData(double *, int, int);
 
-void randData(double *,int,int);
+void randData(double *, int, int);
 
-void findRowSum(const double *,double *,int,int);
+void findRowSum(const double *, double *, int, int);
 
-void findColSum(const double *,double *,int,int);
+void findColSum(const double *, double *, int, int);
 
-int main(){
+int main()
+{
 	srand(time(0));
 	const int N = 6, M = 8;
 	double data[N][M] = {};
 	double sum1[N] = {};
 	double sum2[M] = {};
 	double *dPtr = data[0];
-	randData(dPtr,N,M);
-	showData(dPtr,N,M);
-	
+	randData(dPtr, N, M);
+	showData(dPtr, N, M);
+
 	cout << "---------------------------------------------\n";
-	
-	findRowSum(dPtr,sum1,N,M);
-	showData(sum1,N,1);
-	
+
+	findRowSum(dPtr, sum1, N, M);
+	showData(sum1, N, 1);
+
 	cout << "---------------------------------------------\n";
-	
-	findColSum(dPtr,sum2,N,M); 
-	showData(sum2,1,M);
+
+	findColSum(dPtr, sum2, N, M);
+	showData(sum2, 1, M);
+}
+void showData(double A[], int N, int M)
+{
+	cout << fixed << setprecision(2);
+	for (int i = 0; i < N * M; i++)
+	{
+		cout << A[i];
+		if ((i + 1) % M == 0)
+			cout << endl;
+		else
+			cout << " ";
+	}
+}
+
+void randData(double A[], int N, int M)
+{
+	for (int i = 0; i < N * M; i++)
+	{
+		A[i] = rand() % 101 / 100.0;
+	}
+}
+
+void findRowSum(const double A[], double *b, int N, int M)
+{
+	double sum = 0;
+	int j = 0;
+	for (int i = 0; i < M * N; i++)
+	{
+		sum = sum + A[i];
+		*(b + j) = sum;
+		if ((i + 1) % M == 0)
+		{
+			sum = 0;
+			j++;
+		}
+	}
+}
+
+void findColSum(const double A[], double *b, int N, int M)
+{
+	double sum;
+	for (int i = 0; i < M; i++)
+	{
+		sum = A[i];
+		for (int j = 1; j < N; j++)
+		{
+			sum = sum + A[i + (j * M)];
+		}
+		*(b + i) = sum;
+	}
 }
